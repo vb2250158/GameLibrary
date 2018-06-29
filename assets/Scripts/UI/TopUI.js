d39efe1b66a303cc57d1dd04e0cacbd9e9ac74a0
秋雨之忆 <32808497+vb2250158@users.noreply.github.com>
2018-06-29 11:39:13 +0800

    UI优化

---
+++ assets/Scripts/UI/TopUI.js
@@ -0,0 +1,12 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        upSize: 0,
+    },
+
+
+    start() {
+        this.node.y = cc.view.getVisibleSize().height/2 - upSize;
+    },
+});
