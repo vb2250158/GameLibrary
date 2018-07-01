63a54983d4644c857db8459b1a1101df24a49391
秋雨之忆 <32808497+vb2250158@users.noreply.github.com>
2018-07-01 20:40:54 +0800

    创建点增加

--- assets/Scripts/ObjectFuntion/DestroyByValue.js
+++ assets/Scripts/ObjectFuntion/DestroyByValue.js
@@ -1,13 +1,3 @@
-// Learn cc.Class:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
-
 cc.Class({
     extends: cc.Component,
 
@@ -41,7 +31,7 @@ cc.Class({
      * 值减少
      * @param {*} size 
      */
-    minusNumber(size) {
+    minusNumber() {
         
         for (let index = 0; index < arguments.length; index++) {
             const element = arguments[index];
